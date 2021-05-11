0b929229401ecf050ee42aa1dd2c40fdef9234d9
ZhouChuang <zhouchuang@gmail.com>
2021-05-11 17:50:07 +0800

    second commit

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"./mypkg"
 	"fmt"
+
 )
 
 func main() {
 	mypkg.CustomPkgFunc()
 	fmt.Println("hello world!")
+}
+
+func SayHello()  {
+	fmt.Println("Hello mother fucker...")
 }
